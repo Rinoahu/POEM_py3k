9fa48f5a99b5ebdd5930f4393fd768f908ab9642
Rinoahu <xiaohu@iastate.edu>
2019-03-25 13:57:30 -0500

    improve README

---
+++ upload.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf ./database/cog
+find ./ -name *un~ | xargs rm
+cd example
+sh clean.sh
+cd ..
+
+git config --global user.email xiaohu@iastate.edu
+git config --global user.name Rinoahu
+
+
+git remote rm origin
+
+git add -A .
+git commit -m 'improve README'
+git remote add origin https://github.com/Rinoahu/POEM_py3k
+git pull origin master
+git push origin master
+
+git checkout master
